b8cfaaa39ebc6378b41d0109383f37c45fc63286
Khanaru220 <calsharp0202@yahoo.com>
2024-09-10 12:21:58 +0700

    edit file .sh: remove git stash + add another commit

--- mergePersonalToCompany.sh
+++ mergePersonalToCompany.sh
@@ -6,9 +6,6 @@ SOURCE_BRANCH="personal-ai-street-smart"     # Replace with the branch you are m
 FILE_TO_KEEP1="./slack/slack.yaml"       # Replace with the path to the first file you want to keep
 FILE_TO_KEEP2="./slack/deploy.cloudbuild.yaml"       # Replace with the path to the second file you want to keep
 
-# *Step 1: Stash any current changes (if any)*
-git stash
-
 # *Step 2: Checkout Your Target Branch + Get the Current Commit Hash
 git checkout $TARGET_BRANCH
 CURRENT_COMMIT=$(git rev-parse HEAD)
@@ -22,14 +19,6 @@ git checkout $CURRENT_COMMIT -- $FILE_TO_KEEP1 $FILE_TO_KEEP2
 # *Step 5: Verify Changes (Optional)*
 echo "___The following files have been preserved: $FILE_TO_KEEP1 and $FILE_TO_KEEP2" 
 
-# *Step 6: Commit the Changes*
-git add .
-git commit -m "Merged changes but keeping `$FILE_TO_KEEP1`,  `$FILE_TO_KEEP2` unchanged at `$SOURCE_BRANCH`"
-
 # *Step 7: Push the Changes (if necessary)*
 git push origin $TARGET_BRANCH
 echo "___Merge completed successfully!"
-
-# *Step 8: Go back to original code to continue coding (if necessary)*
-git checkout $SOURCE_BRANCH
-git stash apply
